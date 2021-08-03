22fe2cb4b957629c86bf6809d97d52301d86f7e4
thomaslanning <88064893+thomaslanning@users.noreply.github.com>
2021-08-03 13:16:52 -0600

    Initial Upload

---
+++ testnet.psm1
@@ -0,0 +1,29 @@
+function Test-Ping{
+    param (
+        $server,
+        $count
+    )
+    $test = Test-Connection -ComputerName $server -Count $count
+
+    $jitterArray = [System.Collections.ArrayList]@()
+
+    For ($i = 0; $i -le $test.count; $i++) {
+        $jitter = $test.ResponseTime[$i] - $test.ResponseTime[$i - 1]
+        $jitterArray += [System.Math]::Abs($jitter)
+    }
+
+    $stats = $test | Measure-Object ResponseTime -Average -Maximum -Minimum
+
+    $jitterStats = $jitterArray | Measure-Object -Average
+
+    $results = [ordered]@{}
+
+    $results["Count"] = $test.count
+    $results["Packet Loss"] = (($test.count - $count) / $count) * 100
+    $results["Ping Average"] = $stats.Average
+    $results["Ping Min"] = $stats.Minimum
+    $results["Ping Max"] = $stats.Maximum
+    $results["Ping Jitter"] = $jitterStats.Average
+
+    return $results
+    }
