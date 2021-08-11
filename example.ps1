8afb09f0af2417e5d7907b3b6f4be1544b32e056
thomaslanning <88064893+thomaslanning@users.noreply.github.com>
2021-08-11 06:49:23 -0600

    Revert
    Reverted to original format due to newfound issues with previous ping intervals.

--- example.ps1
+++ example.ps1
@@ -1,8 +1,8 @@
 import-module .\testnet.psm1
 
 $testServer = "8.8.8.8"
-$testSeconds = 5
-Write-Host "Testing connection latency, this will take about $testSeconds seconds."
-$result = Test-Ping -server $testServer  -seconds $testSeconds
+$testCount = 30
+Write-Host "Testing connection latency, this may take $testCount seconds or more."
+$result = Test-Ping -server $testServer  -count $testCount
 Write-Host "Latency test complete. The results are below"
 $result
