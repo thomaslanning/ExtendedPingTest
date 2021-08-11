9c3f6cc7f53ded1184d48c9cfe1723a739a015b5
thomaslanning <88064893+thomaslanning@users.noreply.github.com>
2021-08-11 06:50:22 -0600

    Revert
    Reverted to original format due to newfound issues with previous ping intervals.

--- testnet.psm1
+++ testnet.psm1
@@ -1,43 +1,29 @@
-function Test-Ping {
+function Test-Ping{
     param (
         $server,
-        $seconds
+        $count
     )
-
-    $time = Get-Date
-
-    $ping = [System.Collections.ArrayList]@()
+    $test = Test-Connection -ComputerName $server -Count $count -ErrorAction SilentlyContinue
 
     $jitterArray = [System.Collections.ArrayList]@()
 
-    $errors = 0
-
-    while ((NEW-TIMESPAN –Start $time –End (Get-Date)).seconds -lt $seconds) {
-        try {
-            $ping += (Test-Connection -ComputerName $server -count 1 -ErrorAction Stop).ResponseTime
-        }
-        catch {
-            $errors += 1
-        }
-    }
-
-    For ($i = 1; $i -le $ping.count; $i++) {
-        $jitter = $ping[$i] - $ping[$i - 1]
+    For ($i = 0; $i -le $test.count; $i++) {
+        $jitter = $test.ResponseTime[$i] - $test.ResponseTime[$i - 1]
         $jitterArray += [System.Math]::Abs($jitter)
     }
 
-    $stats = $ping | Measure-Object -Average -Maximum -Minimum
+    $stats = $test | Measure-Object ResponseTime -Average -Maximum -Minimum
 
     $jitterStats = $jitterArray | Measure-Object -Average
 
     $results = [ordered]@{}
 
-    $results["Count"] = $ping.count + $errors
-    $results["Packet Loss"] = (1 - ($ping.count / ($ping.count + $errors))) * 100
+    $results["Count"] = $test.count
+    $results["Packet Loss"] = (($test.count - $count) / $count) * 100
     $results["Ping Average"] = $stats.Average
     $results["Ping Min"] = $stats.Minimum
     $results["Ping Max"] = $stats.Maximum
     $results["Ping Jitter"] = $jitterStats.Average
 
     return $results
-}
+    }
